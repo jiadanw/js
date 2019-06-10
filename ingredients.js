15a32f955376b7ebda8fdafd3f0db6f5f6ba7ab7
JiadanWang <jiadanwang@gmail.com>
2019-06-10 04:32:51 +0000

    ingredients

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+ingredients. forEach(function(ingredients){
+  console.log(ingredients);
+});
+
